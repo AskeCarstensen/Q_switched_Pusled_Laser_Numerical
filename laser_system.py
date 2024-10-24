a7cd08d3d09385ab392919e54e65836db8ae3493
AskeSCarstensen <asc@videometer.com>
2024-10-24 20:18:42 +0200

    a

--- laser_system.py
+++ laser_system.py
@@ -11,32 +11,33 @@ log.basicConfig(
 
 # Constants
 log.info('Defining constants.')
-sigma_LA = 2.8e-23                  # Laser emission cross section [m^2]ø
-sigma_SA = 57e-23                   # Absorber cross section [m^2]
-t_sp = 230e-6                       # Spontaneous emission lifetime [s]
-t_1g = 3.5e-6                       # Relaxation time [s]
-t_21 = 100e-12                      # Relaxation time [s]
-c = 3e8                             # Speed of light [m/s]
-h = 6.626e-34                       # Planck's constant [J·s]
-wavelength_LA = 1064e-9             # Laser wavelength [m]
-wavelength_pump = 808e-9            # Pump wavelength [m]å
-V_LA = 5e-3 * np.pi * (150e-6)**2   # Volume of laser crystal [m^3]
-N0 = 1e26                           # Absorber density [m^-3]
-L_pump = 5e-3                       # Length of laser crystal [m]
-L_cavity = 200e-3                   # Cavity length [m]
-L_absorber = 2e-3                   # Absorber length [m]
-T_out = 0.15                        # Output mirror transmission
-L_passive = 0.03                    # Passive losses per round trip
-w_LA = 150e-6                       # Laser spot size [m]
-w_SA = 150e-6                       # New spot size in saturable absorber [m]
+sigma_LA = 2.8e-23                      # Laser emission cross section [m^2]ø
+sigma_SA = 57e-23                       # Absorber cross section [m^2]
+t_sp = 230e-6                           # Spontaneous emission lifetime [s]
+t_1g = 3.5e-6                           # Relaxation time [s]
+t_21 = 100e-12                          # Relaxation time [s]
+c = 3e8                                 # Speed of light [m/s]
+h = 6.626e-34                           # Planck's constant [J·s]
+wavelength_LA = 1064e-9                 # Laser wavelength [m]
+wavelength_pump = 808e-9                # Pump wavelength [m]å
+ksi_Cr = 0.0017544                      # Cr concentration in saturable absorber
+N0 = 1e26 * ksi_Cr                      # Absorber density [m^-3]
+L_pump = 5e-3                           # Length of laser crystal [m]
+L_cavity = 200e-3                       # Cavity length [m]
+L_absorber = 2e-3                       # Absorber length [m]
+T_out = 0.15                            # Output mirror transmission
+L_passive = 0.03                        # Passive losses per round trip
+w_LA = 150e-6                           # Laser spot size [m]
+w_SA = 150e-6                           # New spot size in saturable absorber [m]
+V_LA = L_pump * np.pi * (220e-6)**2     # Volume of laser crystal [m^3]
 
 # Initial conditions
 log.info('Setting initial conditions')
-N_LA_initial = 0                    # Initial population inversion [m^-3]
-Phi_initial = 1e-40                 # Initial photon flux [m^-2·s^-1]
-N1_initial = N0                     # Initial ground state population in absorber
-N2_initial = 0                      # Initial excited state population in absorber
-Ng_initial = N0                     # Initial ground state population in absorber
+N_LA_initial = 0                        # Initial population inversion [m^-3]
+Phi_initial = 1e-40                     # Initial photon flux [m^-2·s^-1]
+N1_initial = 0                          # Initial ground state population in absorber
+N2_initial = 0                          # Initial excited state population in absorber
+Ng_initial = N0                         # Initial ground state population in absorber
 
 
 def laser_system(t, y, P_pump):
@@ -59,15 +60,14 @@ def laser_system(t, y, P_pump):
     # Adjust photon flux in saturable absorber based on spot sizes
     Phi_SA = Phi * (w_LA / w_SA)**2
 
-    # Total losses per round trip
+    # Losses from the absober per round trip
     L_SA = 2 * (N_g - N2) * sigma_SA * L_absorber
-    L_roundtrip = L_passive + T_out + L_SA
 
     # Photon lifetime in the cavity
-    t_p = (2*L_cavity/c) * 1/L_roundtrip
+    t_p = ( 1 * L_cavity / c ) * 1 / (-np.log((1-T_out)*(1-L_passive)*(1-L_SA)))
 
     # Circulating photon flux equation
-    dPhi_dt = 2 * N_LA * c * sigma_LA * Phi / (L_cavity / 2) - Phi / (t_p) + (N_LA / t_sp) * L_pump * 1e-3
+    dPhi_dt = 2 * N_LA * c * sigma_LA * Phi * (L_pump/(L_cavity)) - Phi / (t_p) + (N_LA / t_sp) * 1e-3
 
     # Energy levels in the saturable absorber
     dNg_dt = -2 * (N_g - N2) * sigma_SA * Phi_SA + N1 / t_1g
@@ -79,9 +79,9 @@ def laser_system(t, y, P_pump):
 
 
 # Range of pump power
-P_pump_values = np.linspace(1, 4.0, 3)  
+P_pump_values = np.linspace(3.0, 4.0, 2)  
 steady_state_results = [] 
-time_span = (0, 5e-4)
+time_span = (0, 2e-3)
 
 #Photon Flux
 plt.figure(figsize=(12, 6))
